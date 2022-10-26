0404cfa05c9d898a7013cb8288dfe4d7ddfa1920
Piero Coronado <pacoronado01@gmail.com>
2022-10-26 17:28:34 -0400

    update_transaction.php

--- update_transaction.php
+++ update_transaction.php
@@ -21,40 +21,74 @@ $mids_to_possibly_update = array(); // will store all the transcation ids to che
 // traverse all records to check for whether to update or delete
 for($i = 0; $i < count($_POST['note']); $i++){
     if(isset($_POST['cdelete'][$i])){
-        array_push($mids_to_delete, $_POST['mid'][$i]); 
+        array_push($mids_to_delete, $_POST['mid'][$i]); // push mids with delete cbox checked
     } else {
         array_push($source_ids, $_POST['sid'][$i]);
         array_push($notes, $_POST['note'][$i]);
-        array_push($mids_to_possibly_update, $_POST['mid'][$i]);
+        array_push($mids_to_possibly_update, $_POST['mid'][$i]); // push mids that didn't have delete cbox checked
+        // will be later used to compare their submitted notes with the database notes, determine if update should occur
     }
 }
 
 $mids_delete_length = count($mids_to_delete);
 if($mids_delete_length > 0) {
-    echo "<h4>Will delete $mids_delete_length records from the table.</h4>";
     for($i = 0; $i < $mids_delete_length; $i++){
         $mid_to_delete = $mids_to_delete[$i];
         $deletion_sql = "delete from CPS3740_2022F.Money_coronapi where cid='$customer_id' and mid='$mid_to_delete'";
         $deletion_result = mysqli_query($con, $deletion_sql);
 
         if($deletion_result){
-            echo "The Code $mid_to_delete has been deleted. <br>";
+            echo "<br>The Code $mid_to_delete has been deleted.";
         } else {
-            echo "Something is wrong with SQL: " . mysqli_error($con);
+            echo "Something is wrong with deletion SQL: " . mysqli_error($con);
         }
     }
-    echo "$mids_delete_length records have been deleted. <br>";
+    echo "<br>$mids_delete_length records have been deleted.";
 } else {
-    echo "No records will be deleted. <br>";
+    echo "<br>No records will be deleted.";
 }
 
 $mids_update_length = count($mids_to_possibly_update);
-echo "Will check $mids_update_length records for possible update.";
-for($i = 0; $i < $mids_update_length; $i++){
-    echo "<br>Record $i - $mids_to_possibly_update[$i] will be checked for possible update.";
-}
+$num_records_updated = 0;
 
+for($i = 0; $i < $mids_update_length; $i++){
+    $mid_to_possibly_update = $mids_to_possibly_updatep[$i];
+    $get_note_for_mid_sql = "select note from CPS3740_2022F.Money_coronapi where cid='$customer_id' and mid='$mid_to_possibly_update'";
+    $get_note_for_mid_result = mysqli_query($con, $get_note_for_mid_sql);
+
+    if($get_note_for_mid_result){
+        $num_notes = mysqli_num_rows($get_note_for_mid_result);
+        if($num_notes > 1) 
+            echo "More than one note was returned for this query.";
+        else if($num_notes == 0)
+            echo "No notes were returned.";
+        else {
+            // get the note from the db for the corresponding mid
+            $db_note = mysqli_fetch_array($get_note_for_mid_result);
+            $db_note = $db_note['note'];
+
+            // compare the note submitted in the form and the note from the db
+            $update_note = (strcmp($db_note, $notes[$i]) == 0) ? false : true;
+
+            if($update_note){
+                $update_sql = "update CPS3740_2022F.Money_coronapi set note='$db_note' where cid='$customer_id' and mid='$mid_to_possibly_update'";
+                $update_result = mysqli_query($con, $update_sql);
+
+                if($update_result){
+                    echo "<br>The Note for code $mid_to_possibly_update has been updated in the database.";
+                    $num_records_updated++;
+                } else {
+                    echo "Something is wrong with update SQL: " . mysqli_error($con);
+                }
+            }
+        }
+    } else {
+        echo "Something is wrong with searching for note SQL: " . mysqli_error($con);
+    }
+
+}
+echo "<br>$num_records_updated records have been updated.";
 // var_dump($_POST);
 
-
+mysqli_close($con);
 ?>
